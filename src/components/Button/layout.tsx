90519843f4fd6a2cb1e53faf1e1e9a93d78b5440
I Made Urip Subagyo <uripsubagyo@Is-MacBook-Pro.local>
2023-10-04 21:41:24 +0700

    Bug Button

--- src/components/Button/layout.tsx
+++ src/components/Button/layout.tsx
@@ -1,22 +1,36 @@
+"use client"
 type ButtonType = {
   name: String;
   className: String;
   onClickFunction?: any;
+  disabled?: boolean;
 };
 
 export default function Button(props: ButtonType) {
   return (
     <>
-      <button
-        className={`bg-[#064C72] p-2 rounded-full text-white
-
+      {props.disabled ? (
+        <button
+          className={`bg-[#064C72] p-2 rounded-full text-white
           ${props.className}
           `}
-        type="button"
-        onClick={props.onClickFunction}
-      >
-        {props.name}
-      </button>
+          type="button"
+          disabled
+          onClick={props.onClickFunction}
+        >
+          {props.name}
+        </button>
+      ) : (
+        <button
+          className={`bg-[#064C72] p-2 rounded-full text-white
+          ${props.className}
+          `}
+          type="button"
+          onClick={props.onClickFunction}
+        >
+          {props.name}
+        </button>
+      )}
     </>
   );
 }
